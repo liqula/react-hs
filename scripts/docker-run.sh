87fcd4454420647f0031601a4a72fe69ae2d6611
Matthias Fischmann <mf@zerobuzz.net>
2017-07-01 16:35:46 +0200

    Pin docker image digest.

--- scripts/docker-run.sh
+++ scripts/docker-run.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
 export DOCKER_IMAGE=fisx/react-hs-docker
+export DOCKER_DIGEST=sha256:affb63c5a1ae66fbccb1edec3ba16191dce580af8f6e1446bc9f013d55623495
 export DOCKER_PATH=/react-hs
 export VNC_PORT=5900
 
 if [ "$1" == "--connect" ]; then
     docker exec -it `docker ps -q --filter="ancestor=$DOCKER_IMAGE" | head -1` /bin/bash
 else
-    docker pull $DOCKER_IMAGE
-    docker run --rm -p $VNC_PORT:$VNC_PORT -it -v `pwd`:$DOCKER_PATH $DOCKER_IMAGE /bin/bash
+    docker pull $DOCKER_IMAGE@$DOCKER_DIGEST
+    docker run --rm -p $VNC_PORT:$VNC_PORT -it -v `pwd`:$DOCKER_PATH $DOCKER_IMAGE@$DOCKER_DIGEST /bin/bash
 fi
