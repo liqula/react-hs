0c105e01fbb975dcbc93e5094e8256b8c0fb42ca
daniel <dvde@gmx.net>
2017-07-31 15:40:45 +0200

    add onFocus handler to todoTextInput, insert tiaValue into current state solves issue #46: example item editing clears previous description

--- react-hs-examples/src/TodoComponents.hs
+++ react-hs-examples/src/TodoComponents.hs
@@ -56,6 +56,8 @@ todoTextInput = mkStatefulView "todo text input" "" $ \curText args ->
              if keyCode evt == 13 && not (T.null curState) -- 13 is enter
                  then (tiaOnSave args curState, Just "")
                  else ([], Nothing)
+        , onFocus $ \_ _ -> simpleHandler $ \curState ->
+            ([], tiaValue args)
         ]
 
 -- | A combinator suitible for use inside rendering functions.
